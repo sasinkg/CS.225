7b91da583ddd574c5fe786fc8c54253070e23abe
sasin <sasinkg2@illinois.edu>
2021-02-21 18:33:59 -0600

    updating

---
+++ mp_stickers/Image.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+#include "cs225/PNG.h"
+#include "cs225/HSLAPixel.h"
+#include "Image.h"
+
+using namespace std;
+
+    void Image::lighten() {
+        for (unsigned i = 0; i < width(); i++) {
+            for (unsigned j = 0; j < height(); j++) {
+                CS225::HSLAPixel & pixel = getPixel(i,j);
+                if (pixel.l <= 0.9) {
+                    pixel.l = pixel.l + 0.1; 
+                } else if (pixel > 0.9) {
+                    pixel.l = 1;
+                }
+            }
+        }
+    }
+    void Image::lighten(double amount) {
+        for (unsigned i = 0; i < width(); i++) {
+                    for (unsigned j = 0; j < height(); j++) {
+                        CS225::HSLAPixel & pixel = getPixel(i,j);
+                        if (pixel.l + amount <= 1) {
+                            pixel.l = pixel.l + amount; 
+                        } else if (pixel + amount > 1) {
+                            pixel.l = 1;
+                        }
+                    }
+                }
+    }
+    void Image::darken() {
+        for (unsigned i = 0; i < width(); i++) {
+                    for (unsigned j = 0; j < height(); j++) {
+                        CS225::HSLAPixel & pixel = getPixel(i,j);
+                        if (pixel.l - 0.1 < 0) {
+                            pixel.l = 1; 
+                        } else {
+                            pixel.l = pixel.l - 0.1;
+                        }
+                    }
+                }
+    }
+    void Image::darken(double amount) {
+
+    }
+    void Image::saturate() {
+        
+    }
+    void Image::saturate(double amount) {
+
+    }
+    void Image::desaturate(){
+
+    }
+    void Image::desaturate(double amount) {
+
+    }
+    void Image::scale(double factor) {
+
+    } 
+    void Image::(unsigned w, unsigned h) {
+
+    }
+    void Image::illinify() {
+
+    }
+    void Image::grayScale() {
+
+    } 
+    void Image::rotateColor() {
+
+    }
--- mp_stickers/Image.h
+++ mp_stickers/Image.h
@@ -5,3 +5,41 @@
 
 #pragma once
 
+#include <iostream>
+#include "cs225/PNG.h"
+#include "cs225/HSLAPixel"
+
+using namespace std;
+
+class Image: public cs225::PNG {
+    /* private:
+        unsigned int xCor, yCor;
+        int flag; */
+    public:
+        /* Image();
+        Image(unsigned w, unsigned h); */
+        
+        void lighten();
+        void lighten(double amount);
+        
+        void darken;
+        void darken(double amount);
+
+        void saturate;
+        void saturate(double amount);
+
+        void desaturate; 
+        void desaturate(double amount);
+
+        void scale(double factor);
+        void scale(unsigned w, unsigned h);
+
+
+        void illinify();
+        void grayScale();
+        void rotateColor(double degrees);
+        
+        /* unsigned get
+        int getFlag() const;
+        void setFlag(int x); */
+}
